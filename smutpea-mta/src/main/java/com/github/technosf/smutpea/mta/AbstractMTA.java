1c8865c88ded9f569f07c81b8cd1465e301b0be0
technosf <github.10.technomation@xoxy.net>
2014-01-24 17:57:35 -0800

    Moving more basics into AbstractMTA

--- smutpea-mta/src/main/java/com/github/technosf/smutpea/mta/AbstractMTA.java
+++ smutpea-mta/src/main/java/com/github/technosf/smutpea/mta/AbstractMTA.java
@@ -30,7 +30,8 @@ import com.github.technosf.smutpea.core.rfc2821.SessionState;
 /**
  * An Abstract {@code MTA}
  * <p>
- * Handles the housekeeping leaving key logic to the implementing {@code MTA} for:
+ * Handles the housekeeping leaving key logic to the implementing {@code MTA}
+ * for:
  * <ul>
  * <li>Processing valid commands passed to the MTA
  * <li>Processing invalid commands passed to the MTA
@@ -43,313 +44,345 @@ import com.github.technosf.smutpea.core.rfc2821.SessionState;
  */
 public abstract class AbstractMTA implements MTA
 {
-	private static final Logger logger = LoggerFactory.getLogger(AbstractMTA.class);
+    private static final Logger logger = LoggerFactory
+            .getLogger(AbstractMTA.class);
 
-	/*
-	 * Constants
-	 */
-	private static final String CONST_MSG_PROCESS = "Processing line:[{}]";
-	private static final String CONST_MSG_NO_CMD = "No command found.";
-	private static final String CONST_MSG_INVLD_SESS_STATE = "Invalid session state:[{}] for command:[{}s]";
-	private static final String CONST_MSG_UNKNOWN_ERR = "Propagating unknown error:[{}]";
+    /*
+     * Constants
+     */
+    private static final String CONST_MSG_PROCESS = "Processing line:[{}]";
+    private static final String CONST_MSG_NO_CMD = "No command found";
+    private static final String CONST_MSG_INVLD_SESS_STATE =
+            "Invalid session state:[{}] for command:[{}s]";
+    private static final String CONST_MSG_UNKNOWN_ERR =
+            "Propagating unknown error:[{}]";
 
-	/**
-	 * Command response format - code, message, extra
-	 */
-	private static final String CONST_FMT_CMD_RESPONSE = "%1$s %2$s %3$s";
+    /**
+     * Command response format - code, message, extra
+     */
+    private static final String CONST_FMT_CMD_RESPONSE = "%1$s %2$s %3$s";
 
-	/**
-	 * The MTA name
-	 */
-	private final String mtaName;
+    /**
+     * The MTA name
+     */
+    private final String mtaName;
 
-	/**
-	 * The domain the MTA is answering for
-	 */
-	private final String mtaDomain;
+    /**
+     * The domain the MTA is answering for
+     */
+    private final String mtaDomain;
 
+    /**
+     * The Session this MTA wraps
+     */
+    protected final Session session;
 
-	/**
-	 * The Session this MTA wraps
-	 */
-	protected final Session session;
+    /**
+     * The current/last response
+     */
+    private String response;
 
-	/**
-	 * The current/last response
-	 */
-	private String response;
-
-
-	/**
-	 * Constructor setting the domain and mta name.
-	 * 
-	 * @param mtaName
-	 *            The MTA name
-	 * @param mtaDomain
-	 *            The domain the MTA is answering for
-	 * 
-	 * @throws MTAException
-	 *             Session creation failed.
-	 */
-	protected AbstractMTA(final String mtaName, final String mtaDomain)
-					throws MTAException
-	{
-		this.mtaName = mtaName;
-		this.mtaDomain = mtaDomain;
-		this.session = new Session(this);
-	}
+    /**
+     * The current/last Reply Code
+     */
+    private ReplyCode replyCode;// = ReplyCode._220;
 
 
-	/*
-	 * ------------------------------------------------------------------------
-	 * 
-	 * Abstract methods
-	 * 
-	 * ------------------------------------------------------------------------
-	 */
-
-	/**
-	 * <em>Process</em> the given valid command.
-	 * <p>
-	 * The implementing {@code MTA} will define how the valid {@code CommandLine} is processed.
-	 * 
-	 * @param commandLine
-	 *            the {@code CommandLine} to process
-	 * @throws MTAException
-	 */
-	protected abstract void processValidCommand(final CommandLine commandLine) throws MTAException;
-
-
-	/**
-	 * <em>Process</em> the given invalid command.
-	 * <p>
-	 * The implementing {@code MTA} will define how the invalid {@code CommandLine} is processed.
-	 * 
-	 * @param commandLine
-	 *            the invalid {@code CommandLine} to process
-	 * @throws MTAException
-	 */
-	protected abstract void processInvalidCommand(final CommandLine commandLine) throws MTAException;
+    /**
+     * Constructor setting the domain and mta name.
+     * 
+     * @param mtaName
+     *            The MTA name
+     * @param mtaDomain
+     *            The domain the MTA is answering for
+     * @throws MTAException
+     *             Session creation failed.
+     */
+    protected AbstractMTA(final String mtaName, final String mtaDomain)
+            throws MTAException
+    {
+        this.mtaName = mtaName;
+        this.mtaDomain = mtaDomain;
+        this.session = new Session(this);
+    }
 
 
-	/**
-	 * <em>Send</em> the given message.
-	 * <p>
-	 * The implementing {@code MTA} will define how the message is processed.
-	 * 
-	 * @param message
-	 *            the message to send
-	 */
-	protected abstract void sendMessage(final String message);
+    /*
+     * ------------------------------------------------------------------------
+     * 
+     * Abstract methods
+     * 
+     * ------------------------------------------------------------------------
+     */
+
+    /**
+     * <em>Process</em> the given valid command.
+     * <p>
+     * The implementing {@code MTA} will define how the valid
+     * {@code CommandLine} is processed.
+     * 
+     * @param commandLine
+     *            the {@code CommandLine} to process
+     * @throws MTAException
+     */
+    protected abstract void processValidCommand(final CommandLine commandLine)
+            throws MTAException;
 
 
-	/*
-	 * ------------------------------------------------------------------------
-	 * 
-	 * AbstractMTA methods
-	 * 
-	 * ------------------------------------------------------------------------
-	 */
-
-	/**
-	 * Sets the MTA response
-	 * 
-	 * @param response
-	 *            the response
-	 */
-	protected final void setResponse(String response)
-	{
-		this.response = response;
-	}
+    /**
+     * <em>Process</em> the given invalid command.
+     * <p>
+     * The implementing {@code MTA} will define how the invalid
+     * {@code CommandLine} is processed.
+     * 
+     * @param commandLine
+     *            the invalid {@code CommandLine} to process
+     * @throws MTAException
+     */
+    protected abstract void processInvalidCommand(final CommandLine commandLine)
+            throws MTAException;
 
 
-	/**
-	 * Sets the MTA response
-	 * 
-	 * @param code
-	 *            the response code
-	 */
-	protected final void setResponse(ReplyCode code)
-	{
-		this.response =
-						String.format(CONST_FMT_CMD_RESPONSE, code.getCode(),
-										code.getDescription(), "");
-	}
+    /**
+     * <em>Send</em> the given message.
+     * <p>
+     * The implementing {@code MTA} will define how the message is processed.
+     * 
+     * @param message
+     *            the message to send
+     */
+    protected abstract void sendMessage(final String message);
 
 
-	/*
-	 * ------------------------------------------------------------------------
-	 * 
-	 * Final MTA methods
-	 * 
-	 * ------------------------------------------------------------------------
-	 */
+    /*
+     * ------------------------------------------------------------------------
+     * 
+     * AbstractMTA methods
+     * 
+     * ------------------------------------------------------------------------
+     */
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#connect(java.lang.String)
-	 */
-	@Override
-	public final void connect()
-	{
-		setResponse(String.format(CONST_FMT_CMD_RESPONSE, ReplyCode._220.getCode(),
-						getMTADomain(), getMTAName(), getMTADateTime()));
-	}
-
-
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @throws MTAException
-	 * @see com.github.technosf.smutpea.core.MTA#processLine(java.lang.String)
-	 */
-	@Override
-	public final void processLine(String line) throws MTAException
-	{
-		logger.debug(CONST_MSG_PROCESS, line);
-
-		try
-		{
-			setResponse(session.process(line));
-		}
-		catch (MTAException e)
-		// Could not process the line
-		{
-			if (e.getCommandLine() == null)
-			// No command found
-			{
-				logger.debug(CONST_MSG_NO_CMD);
-
-				setResponse(ReplyCode._500); // Syntax error, command unrecognized
-
-				return;
-			}
-			else if (SessionStateException.class.isInstance(e.getCause())
-							&& e.getCommandLine() != null)
-			/*
-			 * Command found, but invalid for the current SessionState Send it to the invalid command processor to set
-			 * the response
-			 */
-			{
-				logger.debug(CONST_MSG_INVLD_SESS_STATE,
-								session.getStateTable().getState(),
-								e.getCommandLine());
-
-				processInvalidCommand(e.getCommandLine());
-
-				return;
-			}
-			else
-			// Not a well defined MTA exception, so propagate it up
-			{
-				logger.debug(CONST_MSG_UNKNOWN_ERR, e.getMessage());
-				throw e;
-			}
-		}
-		catch (SmtpLineException e)
-		{
-			logger.debug(e.getMessage());
-			// TODO Auto-generated catch block
-		}
-	} // public final void processLine(String line) throws MTAException
+    /**
+     * Sets the MTA response description
+     * 
+     * @param response
+     *            the response description
+     */
+    private final void setResponseDescription(String response)
+    {
+        this.response = response;
+    }
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#isClosed()
-	 */
-	@Override
-	public final boolean isClosed()
-	{
-		if (session != null &&
-						SessionState.CLOSED == session.getStateTable().getState())
-		{
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#command(com.github.technosf.smutpea.core.rfc2821.Command.CommandLine)
-	 */
-	@Override
-	public final void command(final CommandLine commandLine)
-					throws MTAException
-	{
-		if (commandLine.isValid())
-		// Valid command processor
-		{
-			processValidCommand(commandLine);
-		}
-		else
-		// Invalid command processor
-		{
-			processInvalidCommand(commandLine);
-		}
-	}
+    /**
+     * Sets the MTA ReplyCode and response
+     * 
+     * @param response
+     *            the response
+     */
+    protected final void setResponse(ReplyCode replyCode, String response)
+    {
+        this.replyCode = replyCode;
+        this.response = response;
+    }
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#send()
-	 */
-	@Override
-	public final void send() throws MTAException
-	{
-		sendMessage(session.getBuffer().toString());
-	}
+    /**
+     * Sets the MTA ReplyCode and response
+     * 
+     * @param code
+     *            the response code
+     */
+    protected final void setResponse(ReplyCode replyCode)
+    {
+        setResponse(replyCode,
+                String.format(CONST_FMT_CMD_RESPONSE, replyCode.getCode(),
+                        replyCode.getDescription(), ""));
+    }
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#getMTADomain()
-	 */
-	@Override
-	public final String getMTADomain()
-	{
-		return mtaDomain;
-	}
+    /*
+     * ------------------------------------------------------------------------
+     * 
+     * Final MTA methods
+     * 
+     * ------------------------------------------------------------------------
+     */
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#connect(java.lang.String)
+     */
+    @Override
+    public final void connect()
+    {
+        setResponse(ReplyCode._220, String.format(CONST_FMT_CMD_RESPONSE,
+                ReplyCode._220.getCode(),
+                getMTADomain(), getMTAName(), getMTADateTime()));
+    }
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#getMTAName()
-	 */
-	@Override
-	public final String getMTAName()
-	{
-		return mtaName;
-	}
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws MTAException
+     * @see com.github.technosf.smutpea.core.MTA#processLine(java.lang.String)
+     */
+    @Override
+    public final void processLine(String line) throws MTAException
+    {
+        logger.debug(CONST_MSG_PROCESS, line);
+
+        try
+        {
+            setResponseDescription(session.process(line));
+        }
+        catch (MTAException e)
+        // Could not process the line
+        {
+            if (e.getCommandLine() == null)
+            // No command found
+            {
+                logger.debug(CONST_MSG_NO_CMD);
+
+                setResponse(ReplyCode._500); // Syntax error, command unrecognized
+
+                return;
+            }
+            else if (SessionStateException.class.isInstance(e.getCause())
+                    && e.getCommandLine() != null)
+            /*
+             * Command found, but invalid for the current SessionState Send it to the invalid command processor to set
+             * the response
+             */
+            {
+                logger.debug(CONST_MSG_INVLD_SESS_STATE,
+                        session.getStateTable().getState(),
+                        e.getCommandLine());
+
+                processInvalidCommand(e.getCommandLine());
+
+                return;
+            }
+            else
+            // Not a well defined MTA exception, so propagate it up
+            {
+                logger.debug(CONST_MSG_UNKNOWN_ERR, e.getMessage());
+                throw e;
+            }
+        }
+        catch (SmtpLineException e)
+        {
+            logger.debug(e.getMessage());
+            // TODO Auto-generated catch block
+        }
+    } // public final void processLine(String line) throws MTAException
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#getMTADateTime()
-	 */
-	@Override
-	public final String getMTADateTime()
-	{
-		return new Date().toString();
-	}
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#isClosed()
+     */
+    @Override
+    public final boolean isClosed()
+    {
+        return session != null
+                && SessionState.CLOSED == session.getStateTable().getState();
+    }
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#getResponse()
-	 */
-	@Override
-	public final String getResponse()
-	{
-		return response;
-	}
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#command(com.github.technosf.smutpea.core.rfc2821.Command.CommandLine)
+     */
+    @Override
+    public final void command(final CommandLine commandLine)
+            throws MTAException
+    {
+        if (commandLine.isValid())
+        // Valid command processor
+        {
+            processValidCommand(commandLine);
+        }
+        else
+        // Invalid command processor
+        {
+            processInvalidCommand(commandLine);
+        }
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#send()
+     */
+    @Override
+    public final void send() throws MTAException
+    {
+        sendMessage(session.getBuffer().toString());
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#getMTADomain()
+     */
+    @Override
+    public final String getMTADomain()
+    {
+        return mtaDomain;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#getMTAName()
+     */
+    @Override
+    public final String getMTAName()
+    {
+        return mtaName;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#getMTADateTime()
+     */
+    @Override
+    public final String getMTADateTime()
+    {
+        return new Date().toString();
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#getResponse()
+     */
+    @Override
+    public final String getResponse()
+    {
+        return response;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.MTA#getReplyCode()
+     */
+    @Override
+    public final ReplyCode getReplyCode()
+    {
+        return replyCode;
+    }
 }
--- smutpea-mta/src/main/java/com/github/technosf/smutpea/mta/impl/SinkMTA.java
+++ smutpea-mta/src/main/java/com/github/technosf/smutpea/mta/impl/SinkMTA.java
@@ -22,177 +22,154 @@ import com.github.technosf.smutpea.core.rfc2821.Command.CommandLine;
 import com.github.technosf.smutpea.core.rfc2821.ReplyCode;
 import com.github.technosf.smutpea.mta.AbstractMTA;
 
-
 /**
- * {@code SinkMTA} is a basic MTA that dumps all email passed to it for transfer.
+ * {@code SinkMTA} is a basic MTA that dumps all email passed to it for
+ * transfer.
  * <p>
- * Although {@code SinkMTA} is a valid MTA, no validation of destinations, hosts or email addresses is done - everything
- * is accepted so that to the client, everything looks OK unless the client violates the RFC.
+ * Although {@code SinkMTA} is a valid MTA, no validation of destinations, hosts
+ * or email addresses is done - everything is accepted so that to the client,
+ * everything looks OK unless the client violates the RFC.
  * 
  * @author technosf
  * @since 0.0.1
  * @version 0.0.1
  */
 public final class SinkMTA
-				extends AbstractMTA
-				implements MTA
+        extends AbstractMTA
+        implements MTA
 {
-	private static final Logger logger = LoggerFactory.getLogger(SinkMTA.class);
+    private static final Logger logger = LoggerFactory.getLogger(SinkMTA.class);
 
-	/*
-	 * Constants
-	 */
-	private static final String CONST_MSG_VALID_PROCESSED = "Valid command processed:[{}] giving code:[{}]";
-	private static final String CONST_MSG_INVALID_PROCESSED = "Invalid command processed:[{}] giving code:[{}]";
-	private static final String CONST_MSG_SENT = "Mail sent to /dev/null with code:[{}]";
+    /*
+     * Constants
+     */
+    private static final String CONST_MSG_VALID_PROCESSED =
+            "Valid command processed:[{}] giving code:[{}]";
+    private static final String CONST_MSG_INVALID_PROCESSED =
+            "Invalid command processed:[{}] giving code:[{}]";
+    private static final String CONST_MSG_SENT =
+            "Mail sent to /dev/null with code:[{}]";
 
-	/**
+    /**
      * 
      */
-	private final static String CONST_MTA_NAME = "SinkMTA v1.0.0";
+    private final static String CONST_MTA_NAME = "SinkMTA v1.0.0";
 
-	/**
-	 * Used as a guard.
-	 */
-	private final MTA mta;
-
-	/**
-	 * Reply code
-	 */
-	private ReplyCode replyCode;
+    /**
+     * Reply code
+     */
+    private ReplyCode replyCode;
 
 
-	/**
-	 * Constructor
-	 * 
-	 * @param mtaDomain
-	 * @throws MTAException
-	 */
-	public SinkMTA(final String domain) throws MTAException
-	{
-		super(CONST_MTA_NAME, domain);
-		mta = this;
-		replyCode = ReplyCode._220;
-	}
+    /**
+     * Constructor
+     * 
+     * @param mtaDomain
+     * @throws MTAException
+     */
+    public SinkMTA(final String domain) throws MTAException
+    {
+        super(CONST_MTA_NAME, domain);
+    }
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.impl.AbstractMTA#processValidCommand(com.github.technosf.smutpea.core.rfc2821.Command.CommandLine)
-	 */
-	@Override
-	protected void processValidCommand(CommandLine commandLine)
-					throws MTAException
-	{
-		synchronized (mta)
-		{
-			switch (commandLine.getCommand())
-			{
-				case DATA:
-					replyCode = ReplyCode._354;
-					break;
-				case HELP:
-					replyCode = ReplyCode._214;
-					break;
-				case QUIT:
-					replyCode = ReplyCode._221;
-					break;
-				case RSET:
-				case VRFY:
-				case EXPN:
-				case NOOP:
-				case EHLO:
-				case HELO:
-				case MAIL:
-				case RCPT:
-				default:
-					replyCode = ReplyCode._250;
-					break;
-			}
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.impl.AbstractMTA#processValidCommand(com.github.technosf.smutpea.core.rfc2821.Command.CommandLine)
+     */
+    @Override
+    protected void processValidCommand(CommandLine commandLine)
+            throws MTAException
+    {
+        switch (commandLine.getCommand())
+        {
+            case DATA:
+                replyCode = ReplyCode._354;
+                break;
+            case HELP:
+                replyCode = ReplyCode._214;
+                break;
+            case QUIT:
+                replyCode = ReplyCode._221;
+                break;
+            case RSET:
+            case VRFY:
+            case EXPN:
+            case NOOP:
+            case EHLO:
+            case HELO:
+            case MAIL:
+            case RCPT:
+            default:
+                replyCode = ReplyCode._250;
+                break;
+        }
 
-			logger.debug(CONST_MSG_VALID_PROCESSED, commandLine.getCommand(), replyCode);
+        logger.debug(CONST_MSG_VALID_PROCESSED, commandLine.getCommand(),
+                replyCode);
 
-			setResponse(replyCode);
-		}
-	}
+        setResponse(replyCode);
+    }
 
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.impl.AbstractMTA#processInvalidCommand(com.github.technosf.smutpea.core.rfc2821.Command.CommandLine)
-	 */
-	@Override
-	protected void processInvalidCommand(CommandLine commandLine)
-					throws MTAException
-	{
-		synchronized (mta)
-		{
-			replyCode = ReplyCode._503;
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.impl.AbstractMTA#processInvalidCommand(com.github.technosf.smutpea.core.rfc2821.Command.CommandLine)
+     */
+    @Override
+    protected void processInvalidCommand(CommandLine commandLine)
+            throws MTAException
+    {
+        replyCode = ReplyCode._503;
 
-			if (commandLine != null
-							&& commandLine.getCommand() != null)
-			{
-				switch (commandLine.getCommand())
-				{
-					case RSET:
-						replyCode = ReplyCode._501;
-						break;
-					case VRFY:
-						replyCode = ReplyCode._501;
-						break;
-					case EHLO:
-					case HELO:
-					case MAIL:
-					case RCPT:
-					case DATA:
-					case EXPN:
-					case HELP:
-					case NOOP:
-					case QUIT:
-					default:
-						replyCode = ReplyCode._503;
-						break;
-				}
-			}
+        if (commandLine != null
+                && commandLine.getCommand() != null)
+        {
+            switch (commandLine.getCommand())
+            {
+                case RSET:
+                    replyCode = ReplyCode._501;
+                    break;
+                case VRFY:
+                    replyCode = ReplyCode._501;
+                    break;
+                case EHLO:
+                case HELO:
+                case MAIL:
+                case RCPT:
+                case DATA:
+                case EXPN:
+                case HELP:
+                case NOOP:
+                case QUIT:
+                default:
+                    replyCode = ReplyCode._503;
+                    break;
+            }
+        }
 
-			logger.debug(CONST_MSG_INVALID_PROCESSED, commandLine.getCommand(), replyCode);
+        logger.debug(CONST_MSG_INVALID_PROCESSED, commandLine.getCommand(),
+                replyCode);
 
-			setResponse(replyCode);
-		}
-	}
+        setResponse(replyCode);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.impl.AbstractMTA#sendMessage(java.lang.String)
-	 */
-	@Override
-	protected final void sendMessage(final String message)
-	{
-		synchronized (mta)
-		{
-			// Do nothing.
-			replyCode = ReplyCode._250;
-			setResponse(String.format("%1$s %2$s", getReplyCode().getCode(),  // TODO validate
-							"Mail sent to /dev/null"));
-			logger.info(CONST_MSG_SENT, getReplyCode().getCode());
-		}
-	}
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see com.github.technosf.smutpea.core.MTA#getReplyCode()
-	 */
-	@Override
-	public final ReplyCode getReplyCode()
-	{
-		synchronized (mta)
-		{
-			return replyCode;
-		}
-	}
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.github.technosf.smutpea.core.impl.AbstractMTA#sendMessage(java.lang.String)
+     */
+    @Override
+    protected final void sendMessage(final String message)
+    {
+        // Do nothing.
+        setResponse(ReplyCode._250,
+                String.format("%1$s %2$s", getReplyCode().getCode(), // TODO validate
+                        "Mail sent to /dev/null"));
+        logger.info(CONST_MSG_SENT, getReplyCode().getCode());
+    }
 
 }
